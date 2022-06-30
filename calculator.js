86101b8e84a6d800678b8418a832da366183c52d
MAVERICSYSTEMS\shrutiv <shrutidewalkar77@gmail.com>
2022-06-30 16:24:56 +0530

    “Adding addition feature to the app”

---
+++ calculator.js
@@ -0,0 +1,7 @@
+
+}
+function add() {
+    var num1 = document.getElementById("firstNumber").value;
+    var num2 = document.getElementById("secondNumber").value;
+    document.getElementById("result").value = Number(num1) + Number(num2);
+}
